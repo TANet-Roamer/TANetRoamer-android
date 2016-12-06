7af5a027190b089d9b313502e052c046d3fee5f4
DanSnow <dododavid006@gmail.com>
2016-12-06 22:17:47 +0800

    refactor: Use interface

--- app/src/main/java/org/aliangliang/tanetroamer/Callback.java
+++ app/src/main/java/org/aliangliang/tanetroamer/Callback.java
@@ -1,16 +1,5 @@
 package org.aliangliang.tanetroamer;
 
-import java.util.concurrent.Callable;
-
-public class Callback implements Callable<String> {
-
-    public String call(String param) throws Exception {
-        return param;
-    }
-
-    @Override
-    public String call() throws Exception {
-        return null;
-    }
-
+public interface Callback {
+    public String call(String param) throws Exception;
 }
